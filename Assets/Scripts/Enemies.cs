a4cd3a15821d6df0616fe3e3b16c7935e7ab8dad
Tadeáš Anděl <tadeas.andel@gmail.com>
2019-04-04 14:55:20 +0200

    ScoreBoard, Enemies exploding and interacting

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreBoard : MonoBehaviour
+{
+    int score = 0;
+    Text scoretext;
+    [SerializeField] int scorePerHit = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretext = GetComponent<Text>();
+        scoretext.text = score.ToString();
+    }
+
+    public void ScoreHit(int scorePerHit)
+    {
+        score += scorePerHit;
+        scoretext.text = score.ToString();
+    }
+}
